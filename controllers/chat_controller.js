384864107f66b06b889ff55d64122e757eb48810
DESKTOP-GBNRKIF\robin <robinson1994@interia.pl>
2017-11-12 13:48:55 +0100

    added user authorization

--- controllers/chat_controller.js
+++ controllers/chat_controller.js
@@ -4,18 +4,27 @@ const Users = require('../models/user')
 const Conversations = require('../models/conversation')
 
 exports.load_chat_data = (req, res) => {
-    Users.find({}, 'username _id', (err, users) => {
-        console.log(req.body)
+    jwt.verify(req.headers.authorization, 'supersecretsecret', (err, decoded) => {
         if(err) {
             res.send({
                 status: "error"
             })
         }
         else {
-            res.send({
-                status: "success",
-                users: users
+            Users.find({_id: { $ne: decoded._id }}, 'username _id', (err, users) => {
+                if(err) {
+                    res.send({
+                        status: "error"
+                    })
+                }
+                else {
+                    res.send({
+                        status: "success",
+                        users: users
+                    })
+                }
             })
         }
     })
+    
 }
