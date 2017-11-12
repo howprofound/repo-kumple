b6d2887d48b964e8bf1cdee4463e52d138a38edd
DESKTOP-GBNRKIF\robin <robinson1994@interia.pl>
2017-11-12 15:15:12 +0100

    added chat joining and disconnecting

--- controllers/chat_controller.js
+++ controllers/chat_controller.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken')
 const Users = require('../models/user')
 const Conversations = require('../models/conversation')
 
+var connectedUsers = []
+
 exports.load_chat_data = (req, res) => {
     jwt.verify(req.headers.authorization, 'supersecretsecret', (err, decoded) => {
         if(err) {
@@ -11,7 +13,7 @@ exports.load_chat_data = (req, res) => {
             })
         }
         else {
-            Users.find({_id: { $ne: decoded._id }}, 'username _id', (err, users) => {
+            Users.find({_id: { $ne: decoded.id }}, 'username _id', (err, users) => {
                 if(err) {
                     res.send({
                         status: "error"
@@ -28,3 +30,64 @@ exports.load_chat_data = (req, res) => {
     })
     
 }
+
+exports.chat_connection = (id, socket) => {
+    socket.emit('connected_users', connectedUsers)
+    connectedUsers.push({
+        id: id,
+        socketId: socket.id
+    })
+    socket.broadcast.emit('user_change', {id: id, isActive: true})
+}
+
+exports.chat_disconnection = socket => {
+    let dcUser
+    connectedUsers = connectedUsers.filter(user => {
+        if(user.socketId !== socket.id)
+            return true
+        else {
+            dcUser = user.id
+            return false
+        }
+    })
+    socket.broadcast.emit('user_change', {id: id, isActive: false})
+    Users.findOneAndUpdate({
+        _id: dcUser
+    }, {
+        $set: { lastLogin: Date.now() }
+    }, err => {
+        if(err) console.log(err)
+    })
+}
+
+/*socket.on("new-message", message => {
+    Messages.create({
+        content: message.content,
+        author: message.author
+    }, (err, messageInstance) => {
+        if(err) console.log(err)
+            console.log(message.author, message.conversation)
+        Conversations.findOneAndUpdate({
+            isPrivate: true,
+            users: { $all: [message.author, message.conversation] }
+        }, {
+            $push: { messages: messageInstance._id}
+        }, err => {
+            if(err) console.log(err)
+            console.log("all ok!")
+            console.log(message.conversation)
+            let target = connectedUsers.find(user => user.id === message.conversation)
+            console.log(target)
+            if(target) {
+                io.to(target.socketId).emit("new-message", {
+                    content: message.content,
+                    author: message.author
+                })
+            }
+            socket.emit("new-message", {
+                content: message.content,
+                author: message.author
+            })
+        })
+    })
+})*/
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express 		= require('express')
 const path 			= require('path')
 const socketioJwt = require('socketio-jwt');
-
+const mongoose = require('mongoose');
 
 const Messages = require('../models/message')
 const Users = require('../models/user')
 const Conversations = require('../models/conversation')
 
-var mongoose = require('mongoose');
+const chat_controller = require('../controllers/chat_controller')
 
 let connectedUsers = []
 module.exports = (app, db, io) => {
@@ -25,78 +25,8 @@ module.exports = (app, db, io) => {
 		handshake: true
 	}))
 
-	io.on('connection', (socket) => {
-		console.log("user connected")
-		socket.on('hello', (id, callback) => {
-			Users.find({_id: {"$ne": id}}, (err, users) => {
-				if(err) console.log(err)
-				else {
-					callback(users.map(user => {
-						return {
-							id: user._id,
-							username: user.username,
-							isActive: connectedUsers.find(cUser => cUser.id == user._id) ? true : false // == because we want to convert _id.toString()
-						}
-					}))
-				}
-				socket.broadcast.emit('user-change', {id: id, isActive: true})
-				connectedUsers.push({
-					id: id,
-					socketId: socket.id
-				})
-				console.log(connectedUsers)
-			})
-		})
-		socket.on("disconnect", () => {
-			let dcUser;
-			connectedUsers = connectedUsers.filter(user => {
-				if(user.socketId !== socket.id)
-					return true
-				else {
-					dcUser = user.id
-					return false
-				}
-			})
-			io.emit('user-change', {id: dcUser, isActive: false})
-			Users.findOneAndUpdate({
-				_id: dcUser
-			}, {
-				$set: { lastLogin: Date.now() }
-			}, err => {
-				if(err) console.log(err)
-			})
-			console.log("user disconnected")
-		})
-		socket.on("new-message", message => {
-			Messages.create({
-				content: message.content,
-				author: message.author
-			}, (err, messageInstance) => {
-				if(err) console.log(err)
-					console.log(message.author, message.conversation)
-				Conversations.findOneAndUpdate({
-					isPrivate: true,
-					users: { $all: [message.author, message.conversation] }
-				}, {
-					$push: { messages: messageInstance._id}
-				}, err => {
-					if(err) console.log(err)
-					console.log("all ok!")
-					console.log(message.conversation)
-					let target = connectedUsers.find(user => user.id === message.conversation)
-					console.log(target)
-					if(target) {
-						io.to(target.socketId).emit("new-message", {
-							content: message.content,
-							author: message.author
-						})
-					}
-					socket.emit("new-message", {
-						content: message.content,
-						author: message.author
-					})
-				})
-			})
-		})
+	io.on('connection', socket => {
+		socket.on("join", id => chat_controller.chat_connection(id, socket))
+		socket.on("disconnect", id => chat_controller.chat_disconnection(id, socket))
 	})
 }
